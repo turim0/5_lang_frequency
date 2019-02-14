c2984417d9dce1f84bb275ea320866da2cceeeca
turim0 <ivan_medvedev_1996@mail.ru>
2019-02-14 14:31:59 +0300

    first version of script

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+Program allows you to determine the 10 most common words in text
 
+# Quickstart
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+
+$ python lang_frequency.py <filepath>  # possibly requires call of python3 executive instead of just python
+The word: "в" occurs 6 times
+The word: "на" occurs 5 times
+The word: "необходимо" occurs 5 times
+The word: "для" occurs 5 times
+The word: "и" occurs 4 times
+The word: "задачи" occurs 4 times
+The word: "сервис" occurs 3 times
+The word: "или" occurs 3 times
+The word: "идентификатор" occurs 3 times
+The word: "задание" occurs 2 times
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,40 @@
+import sys
+import re
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath, encoding='utf-8') as text_file:
+            text = text_file.read()
+            return text
+    except (FileNotFoundError, UnicodeDecodeError):
+        return None
 
 
-def get_most_frequent_words(text):
-    pass
+def get_clean_list_of_words(text):
+    pattern = re.compile(r'[^a-zA-Zа-яА-Я\s]')
+    clean_text = pattern.sub('', text).lower()
+    list_of_words = re.split(r'\s+', clean_text)
+    return list_of_words
+
+
+def get_most_frequent_words(list_of_words):
+    ten_most_frequent_words = Counter(list_of_words).most_common(10)
+    return ten_most_frequent_words
+
+
+def print_most_frequent_words(ten_most_frequent_words):
+    for i in ten_most_frequent_words:
+        print('The word "{0}" occurs {1} times'.format(i[0], i[1]))
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 1 or sys.argv[1] in {'-h', '--help'}:
+        print('Usage: {0} + your_file.json'.format(sys.argv[0]))
+        sys.exit()
+    text = load_data(sys.argv[1])
+    if text is None:
+        sys.exit('No such file or decode error')
+    ten_most_frequent_words = get_most_frequent_words(get_clean_list_of_words(text))
+    print_most_frequent_words(ten_most_frequent_words)
